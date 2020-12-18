cf3aaed7a7b73331279c99c6ab82df893793a1a3
Garcy <garcyv@gmail.com>
2020-12-18 10:40:02 -0300

    Binding

--- app/src/main/java/com/example/ejercicio_14_dic_2020/MainActivity.java
+++ app/src/main/java/com/example/ejercicio_14_dic_2020/MainActivity.java
@@ -1,33 +1,32 @@
 package com.example.ejercicio_14_dic_2020;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ejercicio_14_dic_2020.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText nom,ape;
+
     private String label=MainActivity.class.getSimpleName();
 
+    private ActivityMainBinding binding;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        //recuperamos los objetos editores texto
-        nom = findViewById(R.id.edtNombre);
-        ape = findViewById(R.id.edtApellido);
         Log.d( label, "OnCreate");
 
-        Button mButton =findViewById(R.id.myButton);
-        mButton.setOnClickListener(new View.OnClickListener() {
+        binding.myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "PRESS", Toast.LENGTH_SHORT).show();
